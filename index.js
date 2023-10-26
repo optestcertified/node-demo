738c4978dd4ec5c73c99b0e60823762efc28f7ed
opy <140424822+optestcertified@users.noreply.github.com>
2023-10-27 00:52:19 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 9000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Welcome it works!')
+})
+
+app.get('/api', (req, res) => {
+  res.send('Api is LIVE!!!')
 })
 
 app.listen(port, () => {
